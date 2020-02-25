dd04dbef84c2aad1c8fd970b7fb9eb13ef569018
kingsley <kingsley2036@qq.com>
2020-02-25 20:13:55 +0800

    添加icon和iconposition属性

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,58 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
 </template>
 
 <script>
     export default {
-        // name: "button"
+        props: ['icon', 'icon-position']
     }
 </script>
 
 <style scoped lang="scss">
-    .g-button{
+    .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 1.2em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover{
-            border-color:var(--border-color-hover);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover {
+            border-color: var(--border-color-hover);
         }
-        &:active{
-            background:var(--button-active-bg);
+        &:active {
+            background: var(--button-active-bg);
         }
-        &:focus{
-            outline:none;
+        &:focus {
+            outline: none;
+        }
+        .icon {
+            order: 1;
+            margin-right: 0.1em;
+        }
+        .content{
+            order: 2;
+        }
+        &.icon-right {
+            .icon {
+                order: 2;
+                margin-left: 0.2em;
+                margin-right: 0;
+            }
+            .content {
+                order: 1;
+            }
         }
     }
 
