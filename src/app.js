c4ef58feffad050e3a2dcea97854d5ccd25423df
kingsley <kingsley2036@qq.com>
2020-02-25 20:56:57 +0800

    封装icon组件

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import button from "./button";
+import icon from "./icon";
 Vue.component('g-button',button);
+Vue.component('g-icon',icon);
 new Vue({
     el:'#app',
     // components:{
