669569d0d8ae2af3a7119d44cc1042bc92794550
kingsley <kingsley2036@qq.com>
2020-02-25 23:59:51 +0800

    创建button-group

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import button from "./button";
 import icon from "./icon";
+import buttonGroup  from "./button-group";
 Vue.component('g-button',button);
 Vue.component('g-icon',icon);
+Vue.component('g-button-group',buttonGroup);
 new Vue({
     el:'#app',
     data:{
