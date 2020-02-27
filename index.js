e977c7ae458e0085a8825bfaaaffd3201b9ce940
kingsley <kingsley2036@qq.com>
2020-02-28 00:08:15 +0800

    发布kw-ui到npm上

---
+++ index.js
@@ -0,0 +1,6 @@
+import Button from './src/button'
+import buttonGroup from './src/button-group'
+import Icon from './src/icon'
+
+// export default {Button,buttonGroup,Icon}
+export {Button,buttonGroup,Icon}
