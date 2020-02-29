1ec82d701ad3643ddbdc400139ab29dd21f6877e
kingsley <kingsley2036@qq.com>
2020-02-29 23:33:29 +0800

    完成row和col雏形

---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "kw-row"
+    }
+</script>
+
+<style scoped>
+.row{
+    display: flex;
+}
+</style>
