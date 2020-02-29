2ca6d959bc192be2d55569e1d0d94d2356992faf
kingsley <kingsley2036@qq.com>
2020-02-29 14:45:50 +0800

    添加新的icon

--- src/input.vue
+++ src/input.vue
@@ -1,15 +1,78 @@
 <template>
-    <div>
-        <input type="text">
+    <div class="wrapper" :class="{error:error}">
+        <input :value="value" type="text" :disabled="disabled" :readonly="readonly">
+        <template v-if="error">
+<!--            <g-icon name="settings"></g-icon>-->
+            <icon name="error"></icon>
+            <span>{{error}}</span>
+        </template>
     </div>
 </template>
 
 <script>
+    import Icon from './icon.vue'
     export default {
-        name: "input"
+        name: "input",
+        components:{
+            icon:Icon
+        },
+        props:{
+            'value':{
+                type:String,
+            },
+            'disabled':{
+                type:Boolean,
+                default:false
+            },
+            'readonly':{
+                type:Boolean,
+                default:false
+            },
+            'error':{
+                type:String,
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
+    $height:32px;
+    $border-color:#999;
+    $border-color-hover:#666;
+    $border-radius:12px;
+    $font-size:12px;
+    $box-shadow-color:rgba(0,0,0,0.5);
+    $red:#f1453d;
+    .wrapper{
+    font-size:$font-size;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        input{
+            height:$height ;
+            border: 1px solid $border-color;
+            border-radius: 4px;
+            padding: 0 8px;
+            font-size: inherit;
+            &:hover{
+                border-color: $border-color-hover;
+            }
+            &:focus{
+                box-shadow: inset 0 1px 3px  $box-shadow-color;
+                outline: none;
+            }
+            &[disabled],&[readonly]{
+                border-color: #bbb;
+                color:#bbb;
+                cursor:not-allowed;
+            }
+        }
+        &.error{
+            input{
+                border-color: $red;
+            }
+
+        }
+    }
 
 </style>
