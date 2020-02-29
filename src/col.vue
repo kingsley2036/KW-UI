67bcde6b3812636539f1b7ee7e31822a5b0005e0
kingsley <kingsley2036@qq.com>
2020-03-01 00:17:06 +0800

    添加offset功能

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[`col-${span}`, offset &&`offset-${offset}`]">
         <slot></slot>
     </div>
 </template>
@@ -9,6 +9,9 @@
         name: "kw-col",
         props:{
             span:{
+                type:[Number,String]
+            },
+            offset:{
                 type:[Number,String]
             }
         }
@@ -25,6 +28,12 @@
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n/24)*100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24)*100%;
             }
         }
     }
